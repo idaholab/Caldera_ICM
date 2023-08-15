57a913b02829f766c0293048d82eb4793822f641
Steven Schmidt <steven.schmidt@inl.gov>
2023-08-15 09:23:16 -0600

    Adding two columns 'EVSE_user_facing_name' and 'EVSE_user_facing_description' to the 'EVSE_inputs.csv' file.
    Also, replaced tabs with spaces in the file 'load_EVSE_inventory.cpp'.

--- source/load_inputs/load_EVSE_inventory.cpp
+++ source/load_inputs/load_EVSE_inventory.cpp
@@ -4,264 +4,287 @@
 #include <fstream>
 #include <filesystem>
 #include <algorithm>
+#include <sstream>
 
 load_EVSE_inventory::load_EVSE_inventory(const std::string& inputs_dir) :
-	EVSE_inv{ this->load(inputs_dir) }
+    EVSE_inv{ this->load(inputs_dir) }
 {
 }
 
 const EVSE_inventory& load_EVSE_inventory::get_EVSE_inventory() const
 {
-	return this->EVSE_inv;
+    return this->EVSE_inv;
 }
 
 const EVSE_level load_EVSE_inventory::string_to_EVSE_level(const std::string& str) const
 {
-	const std::unordered_map<std::string, EVSE_level> map = {
-		{"L1", L1},
-		{"L2", L2},
-		{"DCFC", DCFC}
-	};
+    const std::unordered_map<std::string, EVSE_level> map = {
+        {"L1", L1},
+        {"L2", L2},
+        {"DCFC", DCFC}
+    };
 
-	auto it = map.find(str);
-	if (it != map.end()) {
-		return it->second;
-	}
-	else {
-		throw std::invalid_argument("Invalid EVSE level string");
-	}
+    auto it = map.find(str);
+    if (it != map.end()) {
+        return it->second;
+    }
+    else {
+        throw std::invalid_argument("Invalid EVSE level string");
+    }
 }
 
 const EVSE_phase load_EVSE_inventory::string_to_EVSE_phase(const std::string& str) const
 {
-	const std::unordered_map<std::string, EVSE_phase> map = {
-		{"1", singlephase},
-		{"one", singlephase},
-		{"3", threephase},
-		{"three", threephase}
-	};
+    const std::unordered_map<std::string, EVSE_phase> map = {
+        {"1", singlephase},
+        {"one", singlephase},
+        {"3", threephase},
+        {"three", threephase}
+    };
 
-	auto it = map.find(str);
-	if (it != map.end()) {
-		return it->second;
-	}
-	else {
-		throw std::invalid_argument("Invalid EVSE level string");
-	}
+    auto it = map.find(str);
+    if (it != map.end()) {
+        return it->second;
+    }
+    else {
+        throw std::invalid_argument("Invalid EVSE level string");
+    }
 }
 
 EVSE_inventory load_EVSE_inventory::load(const std::string& inputs_dir)
 {
-	EVSE_inventory EVSE_inv;
+    EVSE_inventory EVSE_inv;
 
-	std::string EVSE_inputs_file = inputs_dir + "/EVSE_inputs.csv";
-	ASSERT(std::filesystem::exists(EVSE_inputs_file), EVSE_inputs_file << " file does not exist");
-	std::ifstream EVSE_inputs_file_handle(EVSE_inputs_file);
+    std::string EVSE_inputs_file = inputs_dir + "/EVSE_inputs.csv";
+    ASSERT(std::filesystem::exists(EVSE_inputs_file), EVSE_inputs_file << " file does not exist");
+    std::ifstream EVSE_inputs_file_handle(EVSE_inputs_file);
 
-	//------------------------------
+    //------------------------------
 
-	std::string line;
-	std::vector<std::string> elements_in_line;
-	int line_number = 1;
+    std::string line;
+    std::vector<std::string> elements_in_line;
+    int line_number = 1;
 
-	//-------------------------------
+    //-------------------------------
 
-	// column names
-	std::getline(EVSE_inputs_file_handle, line);
-	elements_in_line = tokenize(line);
+    // column names
+    std::getline(EVSE_inputs_file_handle, line);
+    elements_in_line = tokenize(line);
 
-	// check number of columns
-	ASSERT(elements_in_line.size() == 8, EVSE_inputs_file << " invalid number of columns. Need 8 \
-		columns but " << elements_in_line.size() << " provided in line number " << line_number);
+    // Correct columns.
+    std::vector<std::string> column_names =
+    {
+        "EVSE_type",
+        "EVSE_level",
+        "EVSE_phase_connection",
+        "AC/DC_power_limit_kW",
+        "AC/DC_voltage_limits_V",
+        "AC/DC_current_limit_A",
+        "standby_real_power_kW",
+        "standby_reactive_power_kVAR",
+        "EVSE_user_facing_name",
+        "EVSE_user_facing_description"
+    };
 
-	// check if columns equal to { EVSE_type, EVSE_level, EVSE_phase_connection, AC/DC_power_limit_kW, 
-	// AC/DC_voltage_limits_V, AC/DC_current_limit_A, standby_real_power_kW, standby_reactive_power_kVAR }
-	std::vector<std::string> column_names = { "EVSE_type", "EVSE_level", "EVSE_phase_connection", 
-		"AC/DC_power_limit_kW", "AC/DC_voltage_limits_V", "AC/DC_current_limit_A", "standby_real_power_kW", 
-		"standby_reactive_power_kVAR" };
+    // Check number of columns
+    ASSERT( \
+        elements_in_line.size() == column_names.size(), \
+        EVSE_inputs_file << " invalid number of columns. Need " << column_names.size() << " columns but " << elements_in_line.size() << " provided in line number " << line_number \
+    );
 
-	ASSERT(elements_in_line == column_names, EVSE_inputs_file << " column names doesn\'t match. " <<
-		"Expected column names are { EVSE_type, EVSE_level, EVSE_phase_connection, AC/DC_power_limit_kW, " <<
-		"AC/DC_voltage_limits_V, AC/DC_current_limit_A, standby_real_power_kW, standby_reactive_power_kVAR }");
+    // Check we have the correct columns
+    {
+        std::stringstream assert_error_ss;
+        assert_error_ss << EVSE_inputs_file << " column names don\'t match. " << "Expected column names are { ";
+        for( int i = 0; i < column_names.size(); i++ )
+        {
+            assert_error_ss << column_names.at(i);
+            if( i < column_names.size()-1 )
+            {
+                assert_error_ss << ", ";
+            }
+        }
+        assert_error_ss << " }";
+        ASSERT( elements_in_line == column_names, assert_error_ss.str() );
+    }
 
-	while (std::getline(EVSE_inputs_file_handle, line))
-	{
-		line_number += 1;
-		elements_in_line = tokenize(line);
-		ASSERT(elements_in_line.size() == 8, EVSE_inputs_file << " invalid number of columns. Need 8 \
-			columns but " << elements_in_line.size() << " provided in line number " << line_number);
+    while (std::getline(EVSE_inputs_file_handle, line))
+    {
+        line_number += 1;
+        elements_in_line = tokenize(line);
+        ASSERT( \
+            elements_in_line.size() == column_names.size(), \
+            EVSE_inputs_file << " invalid number of columns. Need " << column_names.size() << " columns but " << elements_in_line.size() << " provided in line number " << line_number \
+        );
 
+        const EVSE_type type = [&]() {
+            // check if EVSE_type is unique
+            std::string str = elements_in_line[0];
+            ASSERT(EVSE_inv.find(str) == EVSE_inv.end(), EVSE_inputs_file << " " << column_names[0] << " " <<
+                str << " is not unique in line number " << line_number);
+            return str;
+        }();
 
-		const EVSE_type type = [&]() {
+        const EVSE_level level = [&]() {
 
-			// check if EVSE_type is unique
-			std::string str = elements_in_line[0];
-			ASSERT(EVSE_inv.find(str) == EVSE_inv.end(), EVSE_inputs_file << " " << column_names[0] << " " <<
-				str << " is not unique in line number " << line_number);
-			return str;
+            // check if battery_chemistry is equal to LTO, LMO or NMC
+            std::vector<std::string> options = { "L1", "L2", "DCFC" };
+            std::string str = elements_in_line[1];
+            bool is_valid = std::any_of(options.begin(), options.end(),
+                [&str](const std::string& s) { return str == s; });
+            ASSERT(is_valid, EVSE_inputs_file << " " << column_names[1] << " " << str << 
+                " is not one of {L1, L2, DCFC} in line number " << line_number);
 
-		}();
+            return this->string_to_EVSE_level(str);
 
-		const EVSE_level level = [&]() {
+        }();
 
-			// check if battery_chemistry is equal to LTO, LMO or NMC
-			std::vector<std::string> options = { "L1", "L2", "DCFC" };
-			std::string str = elements_in_line[1];
-			bool is_valid = std::any_of(options.begin(), options.end(),
-				[&str](const std::string& s) { return str == s; });
-			ASSERT(is_valid, EVSE_inputs_file << " " << column_names[1] << " " << str << 
-				" is not one of {L1, L2, DCFC} in line number " << line_number);
+        const EVSE_phase phase = [&]() {
 
-			return this->string_to_EVSE_level(str);
+            // check if battery_chemistry is equal to LTO, LMO or NMC
+            std::vector<std::string> options = { "1", "3", "one", "three"};
+            std::string str = elements_in_line[2];
+            std::transform(str.begin(), str.end(), str.begin(), ::tolower);
 
-		}();
+            bool is_valid = std::any_of(options.begin(), options.end(),
+                [&str](const std::string& s) { return str == s; });
+            ASSERT(is_valid, EVSE_inputs_file << " " << column_names[2] << " " << str <<
+                " is not one of {1, 3, one, three} in line number " << line_number);
 
-		const EVSE_phase phase = [&]() {
+            return this->string_to_EVSE_phase(str);
 
-			// check if battery_chemistry is equal to LTO, LMO or NMC
-			std::vector<std::string> options = { "1", "3", "one", "three"};
-			std::string str = elements_in_line[2];
-			std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        }();
 
-			bool is_valid = std::any_of(options.begin(), options.end(),
-				[&str](const std::string& s) { return str == s; });
-			ASSERT(is_valid, EVSE_inputs_file << " " << column_names[2] << " " << str <<
-				" is not one of {1, 3, one, three} in line number " << line_number);
+        const double power_limit_kW = [&]() {
 
-			return this->string_to_EVSE_phase(str);
+            // check if power_limit_kW can be converted to double and also non negative
+            std::string str = elements_in_line[3];
+            double val;
+            bool is_conversion_successful;
+            try {
+                val = std::stod(str);
+                is_conversion_successful = true;
+            }
+            catch (...) {
+                is_conversion_successful = false;
+            }
+            ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[3] << " " <<
+                str << " is not a double in line number " << line_number);
 
-		}();
+            ASSERT(val > 0, EVSE_inputs_file << " " << column_names[3] << " " << str <<
+                " is less than or equal to 0 in line number " << line_number);
+            return val;
 
-		const double power_limit_kW = [&]() {
+        }();
 
-			// check if power_limit_kW can be converted to double and also non negative
-			std::string str = elements_in_line[3];
-			double val;
-			bool is_conversion_successful;
-			try {
-				val = std::stod(str);
-				is_conversion_successful = true;
-			}
-			catch (...) {
-				is_conversion_successful = false;
-			}
-			ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[3] << " " <<
-				str << " is not a double in line number " << line_number);
+        const double volatge_limit_V = [&]() {
 
-			ASSERT(val > 0, EVSE_inputs_file << " " << column_names[3] << " " << str <<
-				" is less than or equal to 0 in line number " << line_number);
-			return val;
+            // check if volatge_limit_V is double and also non negative
+            std::string str = elements_in_line[4];
+            double val;
+            bool is_conversion_successful;
+            try {
+                val = std::stod(str);
+                is_conversion_successful = true;
+            }
+            catch (...) {
+                is_conversion_successful = false;
+            }
+            ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[4] << " " <<
+                str << " is not a double in line number " << line_number);
 
-		}();
+            if(level == DCFC)
+            {
+                ASSERT(val > 0, EVSE_inputs_file << " " << column_names[4] << " " << str <<
+                       " is less than or equal to 0 in line number " << line_number);
+            }
+            else
+            {
+                ASSERT(val > 0 || val == -1, EVSE_inputs_file << " " << column_names[4] << " " << str <<
+                       " is less than or equal to 0 in line number " << line_number);
+            }
 
-		const double volatge_limit_V = [&]() {
+            return val;
 
-			// check if volatge_limit_V is double and also non negative
-			std::string str = elements_in_line[4];
-			double val;
-			bool is_conversion_successful;
-			try {
-				val = std::stod(str);
-				is_conversion_successful = true;
-			}
-			catch (...) {
-				is_conversion_successful = false;
-			}
-			ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[4] << " " <<
-				str << " is not a double in line number " << line_number);
+        }();
 
-			if(level == DCFC)
-			{
-				ASSERT(val > 0, EVSE_inputs_file << " " << column_names[4] << " " << str <<
-					   " is less than or equal to 0 in line number " << line_number);
-			}
-			else
-			{
-				ASSERT(val > 0 || val == -1, EVSE_inputs_file << " " << column_names[4] << " " << str <<
-					   " is less than or equal to 0 in line number " << line_number);
-			}
+        const double current_limit_A = [&]() {
 
-			return val;
+            // check if current_limit_A is double and also non negative
+            std::string str = elements_in_line[5];
+            double val;
+            bool is_conversion_successful;
+            try {
+                val = std::stod(str);
+                is_conversion_successful = true;
+            }
+            catch (...) {
+                is_conversion_successful = false;
+            }
 
-		}();
+            if (level == DCFC)
+            {
+                ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[5] << " " <<
+                       str << " is not a double in line number " << line_number);
+            }
+            else
+            {
+                ASSERT(val > 0 || val == -1, EVSE_inputs_file << " " << column_names[5] << " " << str <<
+                       " is less than or equal to 0 in line number " << line_number);
+            }
 
-		const double current_limit_A = [&]() {
+            return val;
 
-			// check if current_limit_A is double and also non negative
-			std::string str = elements_in_line[5];
-			double val;
-			bool is_conversion_successful;
-			try {
-				val = std::stod(str);
-				is_conversion_successful = true;
-			}
-			catch (...) {
-				is_conversion_successful = false;
-			}
+        }();
 
-			if (level == DCFC)
-			{
-				ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[5] << " " <<
-					   str << " is not a double in line number " << line_number);
-			}
-			else
-			{
-				ASSERT(val > 0 || val == -1, EVSE_inputs_file << " " << column_names[5] << " " << str <<
-					   " is less than or equal to 0 in line number " << line_number);
-			}
+        const double standby_real_power_kW = [&]() {
 
-			return val;
+            // check standby_real_power_kW is double and non negative
+            std::string str = elements_in_line[6];
+            double val;
+            bool is_conversion_successful;
+            try {
+                val = std::stod(str);
+                is_conversion_successful = true;
+            }
+            catch (...) {
+                is_conversion_successful = false;
+            }
+            ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[6] << " " << str <<
+                " is not a int in line number " << line_number);
 
-		}();
+            ASSERT(val >= 0, EVSE_inputs_file << " " << column_names[6] << " " << str <<
+                " is less than or equal to 0 in line number " << line_number);
 
-		const double standby_real_power_kW = [&]() {
+            return val;
 
-			// check standby_real_power_kW is double and non negative
-			std::string str = elements_in_line[6];
-			double val;
-			bool is_conversion_successful;
-			try {
-				val = std::stod(str);
-				is_conversion_successful = true;
-			}
-			catch (...) {
-				is_conversion_successful = false;
-			}
-			ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[6] << " " << str <<
-				" is not a int in line number " << line_number);
+        }();
 
-			ASSERT(val >= 0, EVSE_inputs_file << " " << column_names[6] << " " << str <<
-				" is less than or equal to 0 in line number " << line_number);
+        const double standby_reactive_power_kW = [&]() {
 
-			return val;
+            // check AC_charge_rate_kW is double
+            std::string str = elements_in_line[7];
+            double val;
+            bool is_conversion_successful;
+            try {
+                val = std::stod(str);
+                is_conversion_successful = true;
+            }
+            catch (...) {
+                is_conversion_successful = false;
+            }
+            ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[7] << " " <<
+                str << " is not a double in line number " << line_number);
 
-		}();
+            return val;
+        }();
 
-		const double standby_reactive_power_kW = [&]() {
+        // other checks
 
-			// check AC_charge_rate_kW is double
-			std::string str = elements_in_line[7];
-			double val;
-			bool is_conversion_successful;
-			try {
-				val = std::stod(str);
-				is_conversion_successful = true;
-			}
-			catch (...) {
-				is_conversion_successful = false;
-			}
-			ASSERT(is_conversion_successful, EVSE_inputs_file << " " << column_names[7] << " " <<
-				str << " is not a double in line number " << line_number);
+        EVSE_inv.emplace(type, EVSE_characteristics(type, level, phase, power_limit_kW, volatge_limit_V, 
+            current_limit_A, standby_real_power_kW, standby_reactive_power_kW));
 
-			return val;
-		}();
+    }
 
-		// other checks
-
-		EVSE_inv.emplace(type, EVSE_characteristics(type, level, phase, power_limit_kW, volatge_limit_V, 
-			current_limit_A, standby_real_power_kW, standby_reactive_power_kW));
-
-	}
-
-	return std::move(EVSE_inv);
+    return std::move(EVSE_inv);
 }
