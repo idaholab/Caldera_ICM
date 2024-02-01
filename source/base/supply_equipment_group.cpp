1bec49866cbfa6a1e1cd18a3d5b5593b87657dd4
Steven Schmidt <steven.schmidt@inl.gov>
2024-02-01 14:05:49 -0700

    Fixed initialization bugs in 'datatypes_global.h' and other clean-ups.

--- source/base/supply_equipment_group.cpp
+++ source/base/supply_equipment_group.cpp
@@ -5,13 +5,13 @@
 #include "factory_supply_equipment_model.h"     // factory_supply_equipment_model
 
 
-supply_equipment_group::supply_equipment_group(const SE_group_configuration& SE_group_topology_, 
-                                               factory_supply_equipment_model& SE_factory, 
-                                               factory_EV_charge_model* PEV_charge_factory,
-                                               factory_ac_to_dc_converter* ac_to_dc_converter_factory, 
-                                               pev_charge_profile_library* charge_profile_library,
-                                               get_base_load_forecast* baseLD_forecaster, 
-                                               manage_L2_control_strategy_parameters* manage_L2_control)
+supply_equipment_group::supply_equipment_group( const SE_group_configuration& SE_group_topology_, 
+                                                factory_supply_equipment_model& SE_factory, 
+                                                factory_EV_charge_model* PEV_charge_factory,
+                                                factory_ac_to_dc_converter* ac_to_dc_converter_factory, 
+                                                pev_charge_profile_library* charge_profile_library,
+                                                get_base_load_forecast* baseLD_forecaster, 
+                                                manage_L2_control_strategy_parameters* manage_L2_control )
 {
     this->SE_group_topology = SE_group_topology_;    
     this->SE_objs.clear();
