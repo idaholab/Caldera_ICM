007565003b4f063407fad145b67fcffaebc55aa1
Steven Schmidt <steven.schmidt@inl.gov>
2024-08-05 16:57:57 -0600

    More clean-up in a few more files. (Aug 5, 2024)

--- source/base/Aux_interface.cpp
+++ source/base/Aux_interface.cpp
@@ -40,7 +40,7 @@ std::vector<pev_batterySize_info> get_pevType_batterySize_map()
 //                        Get Charge Profile Object
 //#############################################################################
 
-CP_interface::CP_interface(const std::string& input_path)
+CP_interface::CP_interface( const std::string& input_path )
     : loader{ load_EV_EVSE_inventory{ input_path } },
     inventory{ this->loader.get_EV_EVSE_inventory() },
     CP_library{ load_CP_library(inventory, false) }
@@ -48,7 +48,8 @@ CP_interface::CP_interface(const std::string& input_path)
 }
 
 
-CP_interface::CP_interface(const std::string& input_path, bool save_validation_data)
+CP_interface::CP_interface( const std::string& input_path,
+                            const bool save_validation_data )
     : loader{ load_EV_EVSE_inventory{ input_path } },
     inventory{ this->loader.get_EV_EVSE_inventory() },
     CP_library{ load_CP_library(inventory, save_validation_data) }
@@ -58,9 +59,9 @@ CP_interface::CP_interface(const std::string& input_path, bool save_validation_d
 pev_charge_profile_library CP_interface::load_CP_library( const EV_EVSE_inventory& inventory,
                                                           const bool save_validation_data ) const
 {
-    bool create_charge_profile_library = true;
+    const bool create_charge_profile_library = true;
     factory_charge_profile_library CP_Factory{ inventory };
-    return CP_Factory.get_charge_profile_library(save_validation_data, create_charge_profile_library);
+    return CP_Factory.get_charge_profile_library( save_validation_data, create_charge_profile_library );
 }
 
 double CP_interface::get_size_of_CP_library_MB()
@@ -98,7 +99,7 @@ pev_charge_profile_result CP_interface::find_result_given_startSOC_and_endSOC( c
                                                                                const double endSOC )
 {
     pev_charge_profile* CP_ptr = this->CP_library.get_charge_profile(pev_type, SE_type);
-    return CP_ptr->find_result_given_startSOC_and_endSOC(setpoint_P3kW, startSOC, endSOC);
+    return CP_ptr->find_result_given_startSOC_and_endSOC( setpoint_P3kW, startSOC, endSOC );
 }
 
 
@@ -109,7 +110,7 @@ pev_charge_profile_result CP_interface::find_result_given_startSOC_and_chargeTim
                                                                                    const double charge_time_hrs )
 {
     pev_charge_profile* CP_ptr = this->CP_library.get_charge_profile(pev_type, SE_type);
-    return CP_ptr->find_result_given_startSOC_and_chargeTime(setpoint_P3kW, startSOC, charge_time_hrs);
+    return CP_ptr->find_result_given_startSOC_and_chargeTime( setpoint_P3kW, startSOC, charge_time_hrs );
 }
 
 
@@ -119,10 +120,10 @@ std::vector<pev_charge_profile_result> CP_interface::find_chargeProfile_given_st
                                                                                                     const double startSOC,
                                                                                                     const std::vector<double> endSOC )
 {
-    pev_charge_profile* CP_ptr = this->CP_library.get_charge_profile(pev_type, SE_type);
+    pev_charge_profile* CP_ptr = this->CP_library.get_charge_profile( pev_type, SE_type );
     
     std::vector<pev_charge_profile_result> charge_profile;
-    CP_ptr->find_chargeProfile_given_startSOC_and_endSOCs(setpoint_P3kW, startSOC, endSOC, charge_profile);
+    CP_ptr->find_chargeProfile_given_startSOC_and_endSOCs( setpoint_P3kW, startSOC, endSOC, charge_profile );
     
     return charge_profile;
 }
@@ -134,10 +135,10 @@ std::vector<pev_charge_profile_result> CP_interface::find_chargeProfile_given_st
                                                                                                         const double startSOC,
                                                                                                         const std::vector<double> charge_time_hrs )
 {
-    pev_charge_profile* CP_ptr = this->CP_library.get_charge_profile(pev_type, SE_type);
+    pev_charge_profile* CP_ptr = this->CP_library.get_charge_profile( pev_type, SE_type );
         
     std::vector<pev_charge_profile_result> charge_profile;
-    CP_ptr->find_chargeProfile_given_startSOC_and_chargeTimes(setpoint_P3kW, startSOC, charge_time_hrs, charge_profile);
+    CP_ptr->find_chargeProfile_given_startSOC_and_chargeTimes( setpoint_P3kW, startSOC, charge_time_hrs, charge_profile );
     
     return charge_profile;
 }
@@ -149,7 +150,7 @@ std::vector<pev_charge_fragment> CP_interface::USE_FOR_DEBUG_PURPOSES_ONLY_get_r
                                                                                                    const EVSE_type SE_type ) const
 {
     factory_charge_profile_library CP_Factory{this->inventory};
-    return CP_Factory.USE_FOR_DEBUG_PURPOSES_ONLY_get_raw_charge_profile(time_step_sec, target_acP3_kW, pev_type, SE_type);
+    return CP_Factory.USE_FOR_DEBUG_PURPOSES_ONLY_get_raw_charge_profile( time_step_sec, target_acP3_kW, pev_type, SE_type );
 }
 
 //#############################################################################
@@ -157,14 +158,14 @@ std::vector<pev_charge_fragment> CP_interface::USE_FOR_DEBUG_PURPOSES_ONLY_get_r
 //#############################################################################
 
 
-all_charge_profile_data CP_interface_v2::create_charge_profile_from_model(double time_step_sec, 
-                                                                          EV_type pev_type, 
-                                                                          EVSE_type SE_type, 
-                                                                          double start_soc, 
-                                                                          double end_soc, 
-                                                                          double target_acP3_kW, 
-                                                                          EV_ramping_map ramping_by_pevType_only, 
-                                                                          EV_EVSE_ramping_map ramping_by_pevType_seType)
+all_charge_profile_data CP_interface_v2::create_charge_profile_from_model( const double time_step_sec, 
+                                                                           const EV_type pev_type, 
+                                                                           const EVSE_type SE_type, 
+                                                                           const double start_soc, 
+                                                                           const double end_soc, 
+                                                                           const double target_acP3_kW, 
+                                                                           const EV_ramping_map ramping_by_pevType_only, 
+                                                                           const EV_EVSE_ramping_map ramping_by_pevType_seType )
 {
     all_charge_profile_data return_val;
     return_val.timestep_sec = time_step_sec;
@@ -187,13 +188,13 @@ all_charge_profile_data CP_interface_v2::create_charge_profile_from_model(double
     std::vector<double> soc;
     std::vector<ac_power_metrics> ac_power_vec;
     
-    CP_Factory_v2.create_charge_profile(time_step_sec, pev_SE, start_soc, end_soc, target_acP3_kW, soc, ac_power_vec);
+    CP_Factory_v2.create_charge_profile( time_step_sec, pev_SE, start_soc, end_soc, target_acP3_kW, soc, ac_power_vec );
 
     //---------------------
     
-    int num_elements = soc.size();
+    const int num_elements = soc.size();
     
-    for(int i=0; i<num_elements; i++)
+    for( int i = 0; i < num_elements; i++ )
     {
         return_val.P1_kW.push_back(ac_power_vec.at(i).P1_kW);
         return_val.P2_kW.push_back(ac_power_vec.at(i).P2_kW);
@@ -207,29 +208,32 @@ all_charge_profile_data CP_interface_v2::create_charge_profile_from_model(double
     return return_val;
 }
 
-CP_interface_v2::CP_interface_v2(const std::string& input_path)
+CP_interface_v2::CP_interface_v2( const std::string& input_path )
     : loader{ load_EV_EVSE_inventory{ input_path } },
     inventory{ this->loader.get_EV_EVSE_inventory() },
     CP_library_v2{ pev_charge_profile_library_v2 {this->inventory} } 
 {};
 
 
-CP_interface_v2::CP_interface_v2(const std::string& input_path,
-                                 double L1_timestep_sec, 
-                                 double L2_timestep_sec, 
-                                 double HPC_timestep_sec, 
-                                 EV_ramping_map ramping_by_pevType_only, 
-                                 EV_EVSE_ramping_map ramping_by_pevType_seType)
+CP_interface_v2::CP_interface_v2( const std::string& input_path,
+                                  const double L1_timestep_sec, 
+                                  const double L2_timestep_sec, 
+                                  const double HPC_timestep_sec, 
+                                  const EV_ramping_map ramping_by_pevType_only, 
+                                  const EV_EVSE_ramping_map ramping_by_pevType_seType )
     : loader{ load_EV_EVSE_inventory{ input_path } },
     inventory{ this->loader.get_EV_EVSE_inventory() },
     CP_library_v2{ factory_charge_profile_library_v2{this->inventory, ramping_by_pevType_only, ramping_by_pevType_seType}.get_charge_profile_library(L1_timestep_sec, L2_timestep_sec, HPC_timestep_sec)}
 {
 }
 
-std::vector<double> CP_interface_v2::get_P3kW_charge_profile(double start_soc, double end_soc, EV_type pev_type, EVSE_type SE_type)
+std::vector<double> CP_interface_v2::get_P3kW_charge_profile( const double start_soc,
+                                                              const double end_soc,
+                                                              const EV_type pev_type,
+                                                              const EVSE_type SE_type )
 {
     std::vector<double> return_val;
-    this->CP_library_v2.get_P3kW_charge_profile(start_soc, end_soc, pev_type, SE_type, this->timestep_sec, return_val);
+    this->CP_library_v2.get_P3kW_charge_profile( start_soc, end_soc, pev_type, SE_type, this->timestep_sec, return_val );
     
     return return_val;
 }
@@ -241,10 +245,13 @@ double CP_interface_v2::get_timestep_of_prev_call_sec()
 }
 
 
-all_charge_profile_data CP_interface_v2::get_all_charge_profile_data(double start_soc, double end_soc, EV_type pev_type, EVSE_type SE_type)
+all_charge_profile_data CP_interface_v2::get_all_charge_profile_data( const double start_soc,
+                                                                      const double end_soc,
+                                                                      const EV_type pev_type,
+                                                                      const EVSE_type SE_type )
 {
     all_charge_profile_data return_val;
-    this->CP_library_v2.get_all_charge_profile_data(start_soc, end_soc, pev_type, SE_type, return_val);
+    this->CP_library_v2.get_all_charge_profile_data( start_soc, end_soc, pev_type, SE_type, return_val );
     
     return return_val;
 }
@@ -255,14 +262,20 @@ all_charge_profile_data CP_interface_v2::get_all_charge_profile_data(double star
 //#############################################################################
 
 
-get_baseLD_forecast::get_baseLD_forecast(double data_start_unix_time, int data_timestep_sec, std::vector<double> actual_load_akW, std::vector<double> forecast_load_akW, double adjustment_interval_hrs)
+get_baseLD_forecast::get_baseLD_forecast( const double data_start_unix_time,
+                                          const int data_timestep_sec,
+                                          const std::vector<double> actual_load_akW,
+                                          const std::vector<double> forecast_load_akW,
+                                          const double adjustment_interval_hrs )
 {
-    get_base_load_forecast X(data_start_unix_time, data_timestep_sec, actual_load_akW, forecast_load_akW, adjustment_interval_hrs);
+    get_base_load_forecast X( data_start_unix_time, data_timestep_sec, actual_load_akW, forecast_load_akW, adjustment_interval_hrs );
     this->baseLD_forecaster = X;
 }
 
 
-std::vector<double> get_baseLD_forecast::get_forecast_akW(double unix_start_time, int forecast_timestep_mins, double forecast_duration_hrs)
+std::vector<double> get_baseLD_forecast::get_forecast_akW( const double unix_start_time,
+                                                           const int forecast_timestep_mins,
+                                                           const double forecast_duration_hrs )
 {
     return this->baseLD_forecaster.get_forecast_akW(unix_start_time, forecast_timestep_mins, forecast_duration_hrs);
 }
